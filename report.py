b0a2b704b6284c3da354f3d509c510475e10816c
Kántor Ádám <kantor.a@usa.net>
2024-09-24 09:54:44 +0200

    add hourly chart to report

--- report.py
+++ report.py
@@ -34,23 +34,28 @@ def convert_to_df(data:list[dict] ) -> pd.DataFrame:
     })
 
 
-def process_df(df: pd.DataFrame) -> pd.DataFrame:
+def get_daily_df(df: pd.DataFrame) -> pd.DataFrame:
     result = df.resample('D', on='timestamp').last()
     return result
 
 
-def plot_df(df: pd.DataFrame):    
+def get_hourly_df(df: pd.DataFrame) -> pd.DataFrame:
+    result = df.resample('h', on='timestamp').last()
+    return result
+
+
+def plot_daily_df(df: pd.DataFrame):    
     fig, ax = plt.subplots()
 
     # set datetime axis
     fig.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    fig.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))
+    fig.gca().xaxis.set_major_locator(mdates.DayLocator(interval=2))
 
 
     # add data
     bars = ax.bar(x=df.index, height=df['remaining_seat'], color='#f8bbd0', zorder=3)
     # add labels within box
-    ax.bar_label(bars, label_type='center')
+    ax.bar_label(bars, label_type='center', rotation='vertical', fontsize=6)
     # add gridlines
     ax.yaxis.grid(which='major', color='lightgrey', zorder=0)
     # remove spines
@@ -61,7 +66,44 @@ def plot_df(df: pd.DataFrame):
     # remove y axis ticks
     ax.tick_params(axis=u'both', which=u'both',length=0)
     # format x axis
-    plt.gcf().autofmt_xdate()
+    plt.gcf().autofmt_xdate(rotation=90, ha='center')
+
+
+def plot_hourly_df(df: pd.DataFrame):    
+    #x = pd.date_range(start='2024-09-03', end='2024-12-19', freq='h') # df.index
+    x = df.index
+    y = df['remaining_seat']
+
+    fig, ax = plt.subplots()
+
+    # set datetime axis
+    fig.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    fig.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
+
+
+    # add data
+    ax.plot(x, y, color='#f8bbd0', zorder=3)
+    plt.xticks(pd.date_range(start='2024-09-03', end='2024-12-19', freq='7D'))
+    # add gridlines
+    ax.yaxis.grid(which='major', color='lightgrey', zorder=0)
+    # remove spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    # remove y axis ticks
+    ax.tick_params(axis=u'both', which=u'both',length=0)
+    # format x axis
+    plt.gcf().autofmt_xdate(rotation=90, ha='center')
+    # add y axis limits
+    ax.set_ylim([0, 660])
+    # show last value as annotation
+    value = df['remaining_seat'].iloc[-1]
+    coord_x = df.index[-1] + datetime.timedelta(days=3)
+    coord_y = value - 10
+    ann = ax.annotate(value, [coord_x, coord_y])
+    ann.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='grey'))
+
 
 
 def send_report(markdown_text: str, image_path: str):
@@ -117,13 +159,15 @@ if data is None:
 
 # convert to pandas df
 raw_df = convert_to_df(data)
-df = process_df(raw_df)
+df_daily = get_daily_df(raw_df)
+df_hourly = get_hourly_df(raw_df)
 
 # visualize and save result
-plot_df(df)
+#plot_daily_df(df_daily)
+plot_hourly_df(df_hourly)
 plt.savefig(PATH_TO_IMAGE, dpi=300, bbox_inches='tight')
 # convert to markdown
-markdown_text = df.to_markdown(index=True, tablefmt='pipe', colalign=['center'] * len(df.columns))
+markdown_text = df_daily.to_markdown(index=True, tablefmt='pipe', colalign=['center'] * len(df_daily.columns))
 
 # send report in email
 send_report(markdown_text=markdown_text, image_path=PATH_TO_IMAGE)
